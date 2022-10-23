56ac56957a12893d5b21c42e9805096977fe0d8c
LohiKal <116472988+LohiKal@users.noreply.github.com>
2022-10-23 16:44:27 +0800

    Add files via upload

---
+++ db_backup.ps1
@@ -0,0 +1,6 @@
+﻿$source = 'D:\ATLAS DATABASE BACK-UP\ATLAS'
+#Source is the main backup 
+
+$destination = 'E:\NAV'
+@(Get-ChildItem $source -Filter *.bak | Sort LastWriteTime -Descending)[0] | % { Copy-Item -path $_.FullName -destination $("$destination\Atlas_Latest_backup.bak") -force}
+#destination is the external
